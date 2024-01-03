94a7c71f34515966ed23fea70b139cef05dd6c07
Nikita Sivukhin <sivukhin@sharechat.co>
2024-01-04 01:45:43 +0400

    remove strange tests

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,6 @@ mod io;
 mod vm;
 mod vm_spec;
 
-#[cfg(test)]
-mod tests {
-    use std::fs;
-    use std::io::Write;
-    use crate::ops;
-
-    #[test]
-    fn generate_program() {
-        let mut buf = Vec::with_capacity(1024);
-        buf.append(&mut 0x3000u16.to_be_bytes().to_vec());
-        buf.append(&mut 0b0001_001_001_1_00101u16.to_be_bytes().to_vec());
-        buf.append(&mut 0b0001_000_001_0_00_001u16.to_be_bytes().to_vec());
-        buf.append(&mut 0b0001_000_000_0_00_000u16.to_be_bytes().to_vec());
-        buf.append(&mut 0b0001_000_000_0_00_001u16.to_be_bytes().to_vec());
-        buf.append(&mut 0b0001_000_000_0_00_000u16.to_be_bytes().to_vec());
-        buf.append(&mut 0b1101_000_000_0_00_000u16.to_be_bytes().to_vec());
-        let mut f = fs::OpenOptions::new().create(true).write(true).open("examples/example.obj").unwrap();
-        f.write_all(&buf).unwrap();
-    }
-
-    #[test]
-    fn parse() {
-        println!("{}", ops::Operation::parse(0b1111111100000000).unwrap_err());
-    }
-}
-
 fn main() {
     io::term_setup().unwrap_or_else(|e| panic!("terminal setup failed: {}", e));
     let obj_path = &env::args().skip(1).next().unwrap_or_else(|| panic!("object path must be provided as first argument"));
